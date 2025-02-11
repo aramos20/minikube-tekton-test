1b20a5f3aebdaeda578e268b91eaccf5caae2701
Adrián <adrianjoser20@gmail.com>
2025-02-11 18:23:40 -0400

    Remove unused image parameter from Tekton pipeline and pipelineRun configurations

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 
 def get_db_conn():
     conn = psycopg2.connect(
-        host=os.environ['DB_HOST'],  # Acceso directo con corchetes
+        host=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
         user=os.environ['DB_USER'],
         password=os.environ['DB_PASS']
